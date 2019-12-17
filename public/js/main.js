18b50e8cc786fb8828e60385b779cafd62752fc3
Walter Sebastian Carrera <cwalter1994@gmail.com>
2019-12-17 09:01:26 -0500

    about and contact pages added

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,75 @@
+const myVM = (() => {
+
+    let pieceButtons = document.querySelectorAll('.pieceLink'),
+        lightBox = document.querySelector('.lightBox');
+        shadow = document.querySelector('.shadow');
+
+    function parsePieceData(piece) {
+        let targetImg = lightBox.querySelector('.pieceImage'),
+            targetBody = lightBox.querySelector('.pieceInfo'),
+            targetMore = lightBox.querySelector('.pieceSoftware');
+            targetLink = lightBox.querySelector('.moreLink');
+
+        let bodyContent = `
+            <h2 class="pieceTitle">${piece.title}</h2>
+            <h3 class="pieceCategory">${piece.category}</h3>
+            <hr>
+            <p class="pieceDescription">${piece.info}</p>
+        `;
+
+        targetImg.src = `
+            images/${piece.image}
+        `;
+
+        targetImg.alt = `
+            ${piece.title}
+        `;
+        
+        targetBody.innerHTML = bodyContent; 
+
+        var software = piece.software.split(", ");
+        var icons = "";
+        
+        for (i = 0;i < software.length; i++) {
+            icons+=`
+                <li><img class="softwareIcon" src="images/${software[i]}"></li>
+            `;
+        }
+
+        targetMore.innerHTML = icons;
+
+        targetLink.href = `
+            /${piece.div_id}
+        `;
+
+        lightBox.classList.add('show-lb');
+        shadow.classList.add('show-shadow');
+    }
+
+    function getPieceData(event) {
+        event.preventDefault();
+
+        let url = `/${this.getAttribute('href')}`
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                //console.log(data);
+                parsePieceData(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    pieceButtons.forEach(button => button.addEventListener("click", getPieceData));
+
+    lightBox.querySelector('#close').addEventListener("click", function() {
+        lightBox.classList.remove('show-lb');
+        shadow.classList.remove('show-shadow');
+    });
+})();
+
 function toggle() {
     document.getElementById("nav-toggler").classList.toggle("fa-times");
     document.getElementById("nav-toggler").classList.toggle("fa-bars");
--- routes/index.js
+++ routes/index.js
@@ -6,30 +6,42 @@ const sql = require('../utils/sql');
 router.get('/', (req, res) => {
     // console.log('at the main route');
 
-    let query = 'SELECT P.piece_id AS id, P.div_id, P.piece_image AS image, P.piece_title AS title, C.category_name AS category, P.piece_info AS info, GROUP_CONCAT(S.software_name SEPARATOR ", ") AS software FROM tbl_portfolio_piece AS P INNER JOIN tbl_category AS C ON C.category_id = P.category_id JOIN tbl_piece_software AS PS ON PS.piece_id = P.piece_id JOIN tbl_software AS S ON PS.software_id = S.software_id GROUP BY P.piece_id';
+    let query = "SELECT P.piece_id AS id, P.div_id, P.piece_image AS image, P.piece_title AS title FROM tbl_portfolio_piece AS P";
 
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
 
-        console.log(result);
+        //console.log(result);
 
-        res.render('tiles', { piece: result });
+        res.render('pieces', { piece: result });
     })
 })
 
-// router.get('/:id', (req, res) => {
-//     // console.log('hit a dynamic route');
-//     // console.log(req.params.id);
+router.get('/piece/:id', (req, res) => {
+    // console.log('hit a dynamic route');
+    // console.log(req.params.id);
 
-//     let query = `SELECT * FROM __ WHERE id="${req.params.id}"`;
+    let query = `SELECT P.piece_id AS id, P.div_id, P.piece_image AS image, P.piece_title AS title, C.category_name AS category, P.piece_info AS info, GROUP_CONCAT(S.software_image SEPARATOR ", ") AS software FROM tbl_portfolio_piece AS P INNER JOIN tbl_category AS C ON C.category_id = P.category_id JOIN tbl_piece_software AS PS ON PS.piece_id = P.piece_id JOIN tbl_software AS S ON PS.software_id = S.software_id WHERE P.piece_id="${req.params.id}"`;
 
-//     sql.query(query, (err, result) => {
-//         if (err) {throw err; console.log(err); }
+    sql.query(query, (err, result) => {
+        if (err) {throw err; console.log(err); }
 
-//         //console.log(result);
+        //console.log(result);
 
-//         res.json(result[0]);
-//     })
-// })
+        res.json(result[0]);
+    })
+})
+
+router.get('/about', (req, res) => {
+
+    res.render('about');
+
+})
+
+router.get('/contact', (req, res) => {
+
+    res.render('contact');
+
+})
 
 module.exports = router;
